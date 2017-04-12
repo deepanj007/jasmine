ee7c9f0b80c3bf01b2be6996d148a140bdd263ab
deepanjan <ghosh.deepanj@gmail.com>
2017-04-12 11:51:24 +0530

    service spec modified

--- test/jasmineSpecs/serviceSpecs.js
+++ test/jasmineSpecs/serviceSpecs.js
@@ -6,21 +6,18 @@ describe("Service Specs for Sample Testing", function(){
     beforeEach(inject(function ($rootScope, $controller, _addService_) {
         scope = $rootScope.$new();
         addService = _addService_;
-        $controller('myController', {
-            $scope: scope,
-            adding : {}
-        }); 
     }));
-    
+    it('should contain a service', function(){
+        expect(addService).not.toBe(null);
+    })
+    it('service returns proper value', function(){
+         expect(addService.add(1,2)).toBe(4);
+    })
     it('should send cancel notices whan cancelMeeting is called', function() {
-        var fakeHttpPromise = {
-            success: function() {}
-        };
         spyOn(addService, 'add').and.returnValue(3);
-        addService.add(1,2);
+        //addService.add(1,2);
         result = addService.add(1,2);
 
-        //scope.cancelMeeting('foo', 'bar');
         expect(addService.add).toHaveBeenCalled();
         expect(addService.add).toHaveBeenCalledWith(1,2);
         expect(result).toEqual(3);
